a26e3cd4c197eb8f35e1ac0777fb3e4761c701d9
John Ivison <john@ivison.org>
2018-10-01 01:39:42 +0200

    Commented code

--- devicecounter.py
+++ devicecounter.py
@@ -14,6 +14,7 @@ os.system("echo \"If prompted for a password, it is only used to run the nmap co
 #REPLACE ONLY THE &s     &&&.&&&.&                    
 os.system("sudo nmap -sn 192.168.1.0/24 > devices.log")
 
+#format: [<ip address>, <manufacturer>, <mac address>]
 device_list = []
 
 #format: <device manufacturer> : <count>
@@ -23,14 +24,22 @@ manu_count = {}
 init_time = ""
 
 print("Scan completed successfully.\nOpening file devices.log...")
+
+#Interpreting the output from the nmap command
 with open("devices.log", "r") as d:
     for line in d.readlines():
+        
+        #Recording the time of the scan (non-dynamic)
         if "Starting Nmap" in line:
             init_time = line[45:]
             print("Scan started at: {}".format(init_time))
+        
+        #Looking for the ip address
         elif "Nmap scan report for " in line:            
             rec = False
             ipaddr = ""
+
+            #Garbage spaghetti code looking for everything between the dots (theoretically dynamic)
 
             for character in line:
                 try:
@@ -41,6 +50,7 @@ with open("devices.log", "r") as d:
                     if character == "." and rec == True:
                         ipaddr += character
             
+        #Looking for MAC address and manufacturer
         elif "MAC Address: " in line:
             macaddr = ""
             mac_rec = False
@@ -49,8 +59,11 @@ with open("devices.log", "r") as d:
             manu = ""
             manu_rec = False
             parenth_count = 0
-            
+
             for character in line:
+                
+                #More garbage spaghetti code looking for stuff after a first colon (non-dynamic)
+
                 if character == ":":
                     if mac_rec == False:
                         colon_spotted = True
@@ -66,6 +79,7 @@ with open("devices.log", "r") as d:
                 elif mac_rec == True:
                     macaddr += character
                 
+                #Even more garbage spaghetti code looking for stuff between brackets
 
                 if character == "(":
                     parenth_count += 1
@@ -78,15 +92,16 @@ with open("devices.log", "r") as d:
                 elif manu_rec == True:
                     manu += character
 
-
+            #Adds all of the information the program just found to device_list
             device_list.append([ipaddr, manu, macaddr])
 
+            #Counting the nummber of each manufacturer
             try:
                 manu_count[manu] += 1
             except Exception:
                 manu_count[manu] = 1
 
-        
+        #Looking for the number of devices   
         elif "Nmap done:" in line:
             hostcount = ""
             rec = False
@@ -96,10 +111,22 @@ with open("devices.log", "r") as d:
                 elif rec == True:
                     hostcount = character
                     rec = False
+           
+            #Substracts 1 because nmap always counts an extra device
             hostcount = str(int(hostcount) - 1)
 
+    #Wipes devices.log but also creates a backup
     os.system("cp devices.log devices.log.backup; > devices.log")
 
+#The manu count has commas, which is incompatable with a csv file
+def fix_manu_count():
+    output = "{"
+    for manufacturer in manu_count:
+        output += "{}:{}|".format(manufacturer, manu_count[manufacturer])
+    output = output[:-1] + "}"
+    return output
+
+#Appends all of the nmap data to the csv file
 with open("devicehistory.csv", "a+") as csv:
-    csv.write("\n{},{},{}".format(init_time[:-1], str(int(len(device_list))-1), (manu_count)))
+    csv.write("\n{},{},{}".format(init_time[:-1], str(int(len(device_list))-1), fix_manu_count()))
     
