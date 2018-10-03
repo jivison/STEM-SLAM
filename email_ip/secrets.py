ef5a558638f26ab4bc31b46ad6db9ae7680138c1
John Ivison <john@ivison>
2018-10-03 18:35:15 +0000

    Added automatic email ip

---
+++ email_ip/secrets.py
@@ -0,0 +1,5 @@
+sender_address = "StemLocalAtmosphereMonitor@gmail.com"
+sender_password = "templetonstem"
+sender_server = 'smtp.gmail.com'
+sender_port = 587
+recipient_address = "StemLocalAtmosphereMonitor@gmail.com"
