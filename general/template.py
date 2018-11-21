934a39ee59f74a81cc3c29e1ad01e77ae6ff3996
John Ivison <mattecatte@localhost.localdomain>
2018-11-21 00:28:27 -0800

    Changed_a_lot

--- general/template.py
+++ general/template.py
@@ -23,13 +23,16 @@ def run():
 # If you need multiple data points, please see me
 
 data = {
+    # Don't change this one
     'sensor' : sensor,
 
+    # Change singular_data_point to your data type, eg. device_count
     'singular_data_point' : run(), 
 
 }
 
-with open("{}.json".format(data_type), "w+") as outfile:
+# '../' points to the parent directory. Make sure to update this path if it should need changing
+with open("../data/{}.json".format(data_type), "w+") as outfile:
     json.dump(data, outfile)
 
 print("Finished gathering {} from sensor {} \n".format(data_type, sensor))
