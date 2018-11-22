78e1ac710eb1a789ab5b1692da6da12e02741eca
John Ivison <mattecatte@localhost.localdomain>
2018-11-21 21:16:03 -0800

    Added pipeline to document how code flows

--- general/data_transfer/send_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-data_dir="/home/mattecatte/STEM-SLAM/data"
-remotehost="10.32.230.37"
-
-# scp -r stem-server@$remotehost:/home/stem-server/SLAM_Data/ $data_dir
-
-rsync -avz -e 'ssh' $data_dir stem-server@$remotehost:/home/stem-server/SLAM_Data
